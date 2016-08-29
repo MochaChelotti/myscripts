c6f0d06513396348ce22c46c514a65ddd3ea8106
Michael Chelotti <michael.chelotti@emc.com>
2016-08-29 13:23:05 -0400

    Add files via upload

--- robocopy.ps1
+++ robocopy.ps1
@@ -5,6 +5,8 @@
 #           
 # VERSION : 1.0
 #########################################################################################
+Import-Module WebAdministration
+Import-Module WindowsSearch
 
 $Source=Read-Host "Please type the source directory you want to backup"
 $Destination=Read-Host "Please type the path directory you want to copy the backup files"
@@ -37,7 +39,35 @@ Function Pause{
      Write-Host "Press any key to continue..."
 
      $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
+	
+}Import-Module PSWorkflow
+Import-Module .\ShowUI
+function Search-Twitter ($q)
+{
+	$wc = New-Object Net.Webclient
+	$url = http://search.twitter.com/search.rss?q=$q
+	([xml]$wc.downloadstring($url)).rss.channel.item | select *
+}
 
+$ws = @{
+	WindowStartupLocation = "CenterScreen"
+	Width = 500
+	Height = 500
+}
+
+New-Window @ws -Show {
+	ListBox -Background Black -ItemTemplate {
+		Grid -Columns 55, 300 {
+			Image -Column 0 -Name Image -Margin 5
+			TextBlock -Column 1 -Name Title `
+					  -Margin 5 `
+					  -Foreground White `
+					  -TextWrapping Wrap
+		} | ConvertTo-DataTemplate -Binding @{
+			"Image.Source" = "image_link"
+			"Title.Text" = "title"
+		}
+	} -ItemsSource (Search-Twitter PowerShell)
 }
 
 
